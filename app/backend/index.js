4d8bb75d268dda8e35b0266b31c083df9fe96282
Ashish Chauhan <idlure1@KT0047.local>
2026-04-13 14:44:05 +0530

    test versioned image

--- app/backend/index.js
+++ app/backend/index.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
   } 
   else if (req.url === '/version') {
     res.writeHead(200);
-    res.end(`orbit ${VERSION}`);
+    res.end(`orbit v4 - versioned`);
   } 
   else {
     res.writeHead(404);
